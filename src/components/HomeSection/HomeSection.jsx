50f752e7f7a6e7bdb90b64d21b427caadcc1d0a0
Ephraim <2lanieph@gmail.com>
2026-06-08 10:36:51 +0200

    working on professional info section

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -15,13 +15,29 @@ export default function HomeSection() {
                 <NavBar/>
                 <div className='home-content'>
                     <div className='info-and-pic'>
+                        <div className='professional-info'>
+                            <p className='home-greeting'>Hi, I am</p>
+                            <p className='firstname-and-lastname'>Ephraim <span>Ncube</span></p>
+                            <p className='professional-title'>Full-Stack Developer</p>
+                            <p className='professional-description'>I build modern web and mobile applications that solves real-world problems. I specialize in JavaScript, React, React Native, .NET, SQL, and Firebase (NoSql).
+                             </p>
+                        </div>
+                        
+                        <div className='pic-and-icons'>
 
+                        </div>
+
+                    </div>
+
+                    <div className='home-buttons'>
+                        <button className='home-work-btn'>View My Work</button>
+                        <button className='home-cv-btn'>Download CV</button>
                     </div>
 
                     <div className='stats-container'>
                         {
-                            statsData.map(item=>(
-                                <div className='stats-info'>
+                            statsData.map((item, index)=>(
+                                <div className={index < 3 ?'stats-info':'stats-info-last'}>
                                     <p className='stats-value'>{item.value}</p>
                                     <p className='stats-title'>{item.title}</p>
                                 </div>

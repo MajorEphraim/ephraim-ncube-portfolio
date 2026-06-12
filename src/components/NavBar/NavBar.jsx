f8fb7e663930e3d1262b1538f1d8a3c8fd3526fd
Ephraim <2lanieph@gmail.com>
2026-06-12 14:41:54 +0200

    worked on scrollbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,18 +3,20 @@ import NameComp from '../NameComp/NameComp'
 
 export default function NavBar() { 
     return (
-        <nav className='nav-bar'>
-            <NameComp />
+        <nav className='nav-bar-wrapper'>
+            <div className='nav-bar'>
+                <NameComp />
 
-            <ul className='nav-menu'>
-                <li><a className='nav-link active' href='#home'>Home</a></li>
-                <li><a className='nav-link' href='#projects'>Projects</a></li>
-                <li><a className='nav-link' href='#skills'>Skills</a></li>
-                <li><a className='nav-link' href='#experience'>Experience</a></li>
-                <li><a className='nav-link' href='#contact'>Contact</a></li>
-            </ul>
+                <ul className='nav-menu'>
+                    <li><a className='nav-link active' href='#home'>Home</a></li>
+                    <li><a className='nav-link' href='#projects'>Projects</a></li>
+                    <li><a className='nav-link' href='#skills'>Skills</a></li>
+                    <li><a className='nav-link' href='#experience'>Experience</a></li>
+                    <li><a className='nav-link' href='#contact'>Contact</a></li>
+                </ul>
 
-            <button className='download-btn'>Download CV</button> 
+                <button className='download-btn'>Download CV</button> 
+            </div>
         </nav>
     )
 }
